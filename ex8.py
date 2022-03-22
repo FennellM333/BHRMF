b2cdf35265af264982248d9855fcf8bc23c95634
John Meftah <johnmeftah@yahoo.com>
2022-03-22 12:58:48 -0400

    ex8

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two","three","four"))
+print(formatter.format(True,False,False,True))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+"It's knowing that this can't go on forever",
+"Likely one of us will have to spend some days alone",
+"Maybe we'll get 40 years together",
+"But one day you'll be gone and one day I'll be gone."
+))
