26d34fced466a9ae3bfd53f4eb5e25731d55cd0e
Matthew Fennell <FennellM333@outlook.com>
2022-06-27 11:22:16 -0400

    small edits

--- halofinderI.py
+++ halofinderI.py
@@ -5,11 +5,11 @@ from pynbody import filt, array
 import pandas as pd 
 import pylab 
 
-filenamelist =  '/mnt/data0/jillian/h568/h568.cosmo75.4096gsHsbBH.000240'
+file =  '/mnt/data0/jillian/h568/h568.cosmo75.4096gsHsbBH.000240'
 
-nfilenames = len(filenamelist)
+s = pynbody.load(file)
 
-s = pynbody.load(filenamelist)
+s.keys(file)
 
 #initializes the command to find black holes
 def findBH(s):
@@ -21,13 +21,12 @@ def findBHhalos(s,BH):
     BHhalos = BH['amiga.grp']
     return BHhalos
 
-for i in range(nfilenames)
- #defines parts of the BHhalo finder
-   h = s.halos()
-   s.physical_units()
-   BHfilter = pynbody.filt.LowPass('tform',0.0)
-   BH =  s.stars[BHfilter]
-   BHhalos = findBHhalos(s, BH)
+#
+h = s.halos()  
+s.physical_units()  
+BHfilter = pynbody.filt.LowPass('tform',0.0) 
+BH =  s.stars[BHfilter]  
+BHhalos = findBHhalos(s, BH)
 
-
-print(BHhalos) 
+print(type(BHhalos))
+print(type(BHhalos['tform']))
