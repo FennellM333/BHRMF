d3154f07364cea016b87cfe6eca6fff1d70781ae
Matthew Fennell <FennellM333@outlook.com>
2022-06-27 13:46:23 -0400

    beachhea

---
+++ halofinderII.py
@@ -0,0 +1,29 @@
+import pynbody
+import numpy as np 
+import matplotlib.pyplot as plt 
+from pynbody import filt, array
+import pandas as pd 
+import pylab 
+
+file =  '/mnt/data0/jillian/h568/h568.cosmo75.4096gsHsbBH.000240'
+
+s = pynbody.load(file)
+
+#initializes the command to find black holes
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH
+
+def findBHhalos(s,BH):
+    BHhalos = BH['amiga.grp']
+    return BHhalos
+
+#
+h = s.halos()  
+s.physical_units()  
+BHfilter = pynbody.filt.LowPass('tform',0.0) 
+BH =  s.stars[BHfilter]  
+BHhalos = findBHhalos(s, BH)
+
+print(type(BHhalos))
