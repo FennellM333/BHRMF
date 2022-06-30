12a3dd6acdfc090f3ca37ecffa669ba88e80ed72
Matthew Fennell <FennellM333@outlook.com>
2022-06-30 15:54:58 -0400

    loadable keys

--- halofinderII.py
+++ halofinderII.py
@@ -9,6 +9,9 @@ filenamelist =  '/mnt/data0/jillian/h568/h568.cosmo75.4096gsHsbBH.000240'
 
 s = pynbody.load(filenamelist)
 
+s.loadable_keys()
+BHiordlist = {}
+
 #initializes the command to find black holes
 def findBH(s):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
@@ -26,7 +29,9 @@ BHfilter = pynbody.filt.LowPass('tform',0.0)
 BH =  s.stars[BHfilter]  
 BHhalos = findBHhalos(s, BH)
 
-print(type(BHhalos)
+
+      
+      
 #for center distance, find out what halos as in h1, h2, h3, etc... have the blackholes in them,
 #then make the center of the snapshot the center of each galaxy, then calculate the distance from the center
 #"for i in BHhalos..."  then use that sqrt i^2 j^2 k^2 from marcus' code for distance
