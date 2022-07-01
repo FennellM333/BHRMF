c4870c81653e1f62009b9cd07d80c8b6ecffe663
Matthew Fennell <FennellM333@outlook.com>
2022-07-01 12:02:05 -0400

    shaved off extra to troubleshoot

--- halofinderII.py
+++ halofinderII.py
@@ -7,32 +7,27 @@ import pylab
 
 filenamelist =  '/mnt/data0/jillian/h568/h568.cosmo75.4096gsHsbBH.000240'
 
+# iord? that should give each black hole a unique id to match eventually with their respective halo
+
 s = pynbody.load(filenamelist)
 
 s.loadable_keys()
-BHiordlist = {}
+h = halos()
 
-#initializes the command to find black holes
-def findBH(s):
-    BHfilter = pynbody.filt.LowPass('tform',0.0)
-    BH = s.stars[BHfilter]
-    return BH
+#filters our stars into black holes
 
-def findBHhalos(s,BH):
-    BHhalos = BH['amiga.grp']
-    return BHhalos
+BHfilter = pynbody.filt.LowPass('tform',0.0)
+BH = s.stars[BHfilter] 
 
-#
-h = s.halos()  
-s.physical_units()  
-BHfilter = pynbody.filt.LowPass('tform',0.0) 
-BH =  s.stars[BHfilter]  
-BHhalos = findBHhalos(s, BH)
+#this should filter those black holes further to only those with halos
 
+BHhalos = BH['amiga.grp']
 
-      
+print(BHhalos)
       
 #for center distance, find out what halos as in h1, h2, h3, etc... have the blackholes in them,
 #then make the center of the snapshot the center of each galaxy, then calculate the distance from the center
-#"for i in BHhalos..."  then use that sqrt i^2 j^2 k^2 from marcus' code for distance
 
+
+#Idea for distance from center
+#np.min(h.BHhalo['hyb'])
